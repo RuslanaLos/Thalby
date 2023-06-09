0a0e6ce5089c26d47679a1f650eae6598be0e79a
RuslanaLos <ruslanalos74@gmail.com>
2023-06-09 21:27:24 +0300

    second section

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
     $('.owl-carousel').owlCarousel({
         loop:true,
-        autoplay:true,
+        autoplay:false,
         autoplayTimeout:2000,
         navText: [ '', ' ' ],
         nav: false,
